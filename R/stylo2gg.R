bfb9dd912792c4a3b5257a5c84e45e2e7aa7451c
James Clawson <clawson@gmail.com>
2020-08-31 10:29:00 -0500

    this should fix

--- R/stylo2gg.R
+++ R/stylo2gg.R
@@ -675,8 +675,8 @@ s2g_pca <- function(df_z, df_a, the_class, labeling,
       } else {
         the_plot <- the_plot +
           guides(color = guide_legend(override.aes =
-                  # list(aes(label = "#"),
-                  linetype = silly_guides
+                  list(#aes(label = "#"),
+                  linetype = silly_guides)
           ))
       }
 
